a3a37a07d52540c00f08027a287aec97235cb645
Tejaswi Sharma <tejaswi.sharma@intelligent.com>
2017-05-10 20:57:53 +0530

    Added more deployment files

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -3,10 +3,12 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "dummy-dev1", user: "#{fetch :deploy_user}" , roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+ask :branch, "master"
+set :deploy_to, "/home/#{fetch :deploy_user }/apps/#{fetch :application}/#{fetch :branch}"
 
 
 # role-based syntax
