0f2d03dd3729ee47ffcc1f5bc0324b504b321e4f
Tejaswi Sharma <tejaswi.sharma@intelligent.com>
2017-05-10 21:43:35 +0530

    Added

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,4 +36,9 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public"
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
-
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
