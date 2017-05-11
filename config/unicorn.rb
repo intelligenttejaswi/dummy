0cdcf88d1e4f3049ebeedc8e7ad150a888e3fd9f
Tejaswi Sharma <tejaswi.sharma@intelligent.com>
2017-05-11 14:10:21 +0530

    Added

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 # set path to the application
 app_dir = File.expand_path("../../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+working_directory "#{app_dir}/current"
 
 # Set unicorn options
 worker_processes 2
